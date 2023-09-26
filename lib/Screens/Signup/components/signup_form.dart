a547c1a5fdb812a39a92bbc659fc9e36af2018cd
NAME-NikhilPatil <nikhilpatilhaha@gmail.com>
2023-09-26 06:25:43 +0530

    we need changes

---
+++ lib/Screens/Signup/components/signup_form.dart
@@ -0,0 +1,115 @@
+import 'package:educational_app/Screens/home_screen.dart';
+import 'package:educational_app/Screens/phone_number.dart';
+import 'package:flutter/material.dart';
+
+import '../../../components/already_have_an_account_acheck.dart';
+import '../../../constants.dart';
+import '../../Login/login_screen.dart';
+
+class SignUpForm extends StatelessWidget {
+  const SignUpForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Form(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                bottom: defaultPadding,
+              ),
+              child: TextFormField(
+                textInputAction: TextInputAction.done,
+                // obscureText: true,
+                cursorColor: kPrimaryColor,
+                decoration: const InputDecoration(
+                  hintText: "Your name",
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                    child: Icon(Icons.person),
+                  ),
+                ),
+              ),
+            ),
+            // Padding(
+
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.next,
+              cursorColor: kPrimaryColor,
+              onSaved: (phonenumber) {},
+              decoration: const InputDecoration(
+                hintText: "Your phonenumber",
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.phone),
+                ),
+                // ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+              child: TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                cursorColor: kPrimaryColor,
+                onSaved: (email) {},
+                decoration: const InputDecoration(
+                  hintText: "Your email",
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                    child: Icon(Icons.email),
+                  ),
+                ),
+              ),
+            ),
+            TextFormField(
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              cursorColor: kPrimaryColor,
+              decoration: const InputDecoration(
+                hintText: "Your password",
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+            ),
+            const SizedBox(height: defaultPadding),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginScreen(),
+                  ),
+                );
+              },
+              child: Text("Sign Up".toUpperCase()),
+            ),
+            const SizedBox(height: defaultPadding),
+            AlreadyHaveAnAccountCheck(
+              login: false,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+            const SizedBox(
+              height: 180,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
